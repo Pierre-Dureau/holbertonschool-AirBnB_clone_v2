c8674130f63e42cf0db5f79d21d7e2ccf35e7d9c
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-11 10:52:23 +0200

    Add tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -7,13 +7,13 @@ import pep8
 class test_db_storage(unittest.TestCase):
     """Test for DBStorage"""
 
-    def test_pep8_console(self):
-        """Pep8 console.py"""
+    def test_pep8_db_storage(self):
+        """Pep8 db_storage.py"""
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(["models/engine/db_storage.py"])
         self.assertEqual(p.total_errors, 0, 'fix Pep8')
 
-    def test_docstrings_in_console(self):
+    def test_docstrings_in_db_storage(self):
         """checking for docstrings"""
         self.assertIsNotNone(DBStorage.__doc__)
         self.assertIsNotNone(DBStorage.__init__.__doc__)
