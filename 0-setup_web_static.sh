6428c78dc6dba1aa8f3e5375c3c1dce49809ff84
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-25 09:30:08 +0200

    Prepare the web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,6 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-chgrp -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
