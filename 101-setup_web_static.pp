7b699a5946ad21ac8a92e0a7bb9412ee2be41e34
Pierre Dureau <dureaupierre53@gmail.com>
2022-08-25 14:46:11 +0200

    Web server preparation with puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,41 +1,11 @@
 # Web server preparation with puppet
 
-exec { 'update':
-  command => 'apt-get update',
-  path    => '/usr/bin',
-}
-
-package { 'nginx':
-  ensure  => present,
-  name    => 'nginx',
-  require => Exec['update'],
-}
-
-exec { 'mkdir':
-  command => '/usr/bin/mkdir -p /data/web_static/releases/test/'
-}
-
-exec { 'mkdir2':
-  command => '/usr/bin/mkdir -p /data/web_static/shared/'
-}
-
-exec { 'echo':
-  command => '/usr/bin/echo "Holberton School" > /data/web_static/releases/test/index.html'
-}
-
-exec { 'ln':
-  command => '/usr/bin/ln -sf /data/web_static/releases/test/ /data/web_static/current'
-}
-
-exec { 'chown':
-  command => '/usr/bin/chown -R ubuntu:ubuntu /data/'
-}
-
-exec { 'sed':
-  command => '/usr/bin/sed -i "47i location /hbnb_static {alias /data/web_static/current/; }" /etc/nginx/sites-available/default'
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
+exec { '/usr/bin/env apt-get update': }
+-> exec { '/usr/bin/env apt-get -y install nginx': }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/': }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/shared/': }
+-> exec { '/usr/bin/env echo "Holberton School" > /data/web_static/releases/test/index.html': }
+-> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current': }
+-> exec { '/usr/bin/env chown -R ubuntu:ubuntu /data': }
+-> exec { '/usr/bin/env sed -i "47i location /hbnb_static {alias /data/web_static/current/; }" /etc/nginx/sites-available/default': }
+-> exec { '/usr/bin/env service nginx restart': }
